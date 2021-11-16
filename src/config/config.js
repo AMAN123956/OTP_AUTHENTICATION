3b906df796a4cbb5c30e036acf3bf6b4c824d403
Aman Dixit <56161073+AMAN123956@users.noreply.github.com>
2021-11-16 22:30:46 +0530

    Added config js file

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+exports.PORT = "5000"
+exports.MONGODB_URI = 'your mongodb url'
+exports.NODE_ENV = 'development'
+
+exports.JWT_SECRET = 'littlesecret';
+exports.ORIGIN = 'localhost';
+
+exports.FAST2SMS = 'api_key_from_fast_2_sms'
+exports.ADMIN_PHONE = 'admin_phone_number'
